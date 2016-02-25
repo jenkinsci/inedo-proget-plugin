8106e91a3176580310c295312b94179dafe3dc5a
Andrew Sumner <andrew.sumner@assurity.co.nz>
2016-02-26 11:27:08 +1300

    Sync with original

--- src/main/java/com/inedo/http/HttpEasy.java
+++ src/main/java/com/inedo/http/HttpEasy.java
@@ -45,46 +45,65 @@ import com.inedo.http.HttpEasyReader.Family;
  * http://hc.apache.org/httpcomponents-client-ga/
  * https://github.com/hgoebl/DavidWebb#background
  *
- * Example ======= RestResultReader r = RestRequest.request()
- * .baseURI(databaseURI) .path(viewPath +
- * "?startkey=\"{startkey}\"&endkey=\"{endkey}\") .pathParameters(startKey[0],
- * endKey[0]) .get();
+ * Example 
+ * ======= 
+ * RestResultReader r = RestRequest.request()
+ * 							.baseURI(databaseURI) 
+ * 							.path(viewPath + "?startkey=\"{startkey}\"&endkey=\"{endkey}\") 
+ * 							.urlParameters(startKey[0], endKey[0])
+ * 							.get();
  * 
- * String id = r.jsonPath("rows[0].doc._id").getAsString(); String rev =
- * r.jsonPath("rows[0].doc._rev").getAsString();
+ * String id = r.jsonPath("rows[0].doc._id").getAsString(); 
+ * String rev = r.jsonPath("rows[0].doc._rev").getAsString();
  *
- * Error Handling ============== An IOException is thrown whenever a call
- * returns a response code that is not part of the SUCCESS family (ie 200-299).
+ * Error Handling
+ * ==============
+ * An IOException is thrown whenever a call returns a response code that is not part of the SUCCESS 
+ * family (ie 200-299).
  * 
  * In order to prevent an exception being thrown for an expected response use
- * one of the following methods: • request().doNotFailOn(Integer...
- * reponseCodes) • request().doNotFailOn(Family... responseFamily)
+ * one of the following methods: 
+ *  • request().doNotFailOn(Integer... reponseCodes)
+ *  • request().doNotFailOn(Family... responseFamily)
  * 
- * Authentication ============== supports two formats •
- * http://username:password@where.ever • request().authorization(String
- * username, String password)
+ * Authentication
+ * ============== 
+ * Supports two formats
+ *  • http://username:password@where.ever
+ *  • request().authorization(String username, String password)
  * 
- * Host and Certificate Verification ================================= There is
- * no fine grained control, its more of an all or nothing approach:
+ * Host and Certificate Verification
+ * ================================= 
+ * There is no fine grained control, its more of an all or nothing approach:
  * 
- * RestRequest.withDefaults() .allowAllHosts() .trustAllCertificates()
+ * RestRequest.withDefaults() 
+ * 		.allowAllHosts() 
+ * 		.trustAllCertificates()
  * 
- * Proxy ===== Only basic authentication is supported, although I believe the
- * domain can be added by included "domain/" infront of the username (not
- * tested)
+ * Proxy 
+ * ===== 
+ * Only basic authentication is supported, although I believe the domain can be added by included "domain/" 
+ * in front of the username (not tested)
  * 
- * RestRequest.withDefaults() .proxy(Proxy proxy) .proxyAuth(String userName,
- * String password) .bypassProxyForLocalAddresses(boolean bypassLocalAddresses)
+ * RestRequest.withDefaults() 
+ * 		.proxy(Proxy proxy) 
+ * 		.proxyAuth(String userName, String password) 
+ * 		.bypassProxyForLocalAddresses(boolean bypassLocalAddresses)
  *
  * 
- * Redirects ========= Redirects are NOT automatically followed - at least for
- * REST base calls - even though the documentation for HttpURLConnection says
- * that it should...
+ * Redirects 
+ * ========= 
+ * Redirects are NOT automatically followed - at least for REST base calls - even though the documentation
+ * for HttpURLConnection says that it should...
  * 
  * RestResultReader response = RestRequest.request()
- * .doNotFailOn(Family.REDIRECTION) .path(url) .head();
+ * 		.doNotFailOn(Family.REDIRECTION) 
+ * 		.path(url) 
+ * 		.head();
  * 
- * if (response.getResponseCodeFamily() == Family.REDIRECTION) { ... }
+ * if (response.getResponseCodeFamily() == Family.REDIRECTION) {
+ * 		... 
+ * }
  * 
  */
 public class HttpEasy {
@@ -105,11 +124,12 @@ public class HttpEasy {
 	private String baseURI = "";
 	private String path = "";
 	private Object[] urlParams = new Object[0];
+	private DataContentType dataContentType = DataContentType.AUTO_SELECT;
 	private Object rawData = null;
 	private MediaType rawDataMediaType = null;
 	private Map<String, Object> headers = new LinkedHashMap<String, Object>();
 	private List<Field> fields = new ArrayList<Field>();
-
+	
 	public static HttpEasyDefaults withDefaults() {
 		return new HttpEasyDefaults();
 	}
@@ -142,47 +162,104 @@ public class HttpEasy {
 		this.urlParams = pathParams;
 		return this;
 	}
-
-	public HttpEasy doNotFailOn(Integer... reponseCodes) {
-		this.ignoreResponseCodes.addAll(Arrays.asList(reponseCodes));
-		return this;
-	}
-
-	public HttpEasy doNotFailOn(Family... responseFamily) {
-		this.ignoreResponseFamily.addAll(Arrays.asList(responseFamily));
-		return this;
-	}
-
-	public HttpEasy data(Object data, MediaType mediaType) {
-		if (rawData != null) {
-			throw new InvalidParameterException("Only a single data value can be added");
+	
+	/**
+	 * Sets the content type request property to "multipart/form-data"
+	 * Any data that is required must be added via field(name, value, mediaType) method.
+	 * Files are supported but any other objects must be easily converted to a string value.
+	 */
+	public HttpEasy dataForm() {
+		if (this.dataContentType != DataContentType.AUTO_SELECT) {
+			throw new InvalidParameterException("Content type cannot be changed once set");
 		}
-
-		if (!fields.isEmpty()) {
-			throw new InvalidParameterException("Data cannot be used at the same time as fields");
-		}
-
-		rawDataMediaType = mediaType;
-		rawData = data;
-
+		
+		this.dataContentType = DataContentType.FORM_DATA;
 		return this;
 	}
-
+	
+	/**
+	 * Sets the content type request property to "application/x-www-form-urlencoded"
+	 * Any data that is required must be added via field(name, value) method
+	 * and the value must be easily converted to a string value.
+	 */
+	public HttpEasy urlEncodedForm() {
+		if (this.dataContentType != DataContentType.AUTO_SELECT) {
+			throw new InvalidParameterException("Content type cannot be changed once set");
+		}
+		
+		this.dataContentType = DataContentType.X_WWW_FORM_URLENCODED;
+		return this;
+	}
+	
+	/**
+	 * Add a simple text field, if urlEncodedForm() has been used will try to guess the content type.
+	 * 
+	 * <p>If urlEncodedForm() or dataForm() have not been called then the form type will be auto selected:
+	 * if field containing a file has been added then content type will be set to "multipart/form-data"
+	 * else it will be set to "application/x-www-form-urlencoded"</p>
+	 */
 	public HttpEasy field(String name, Object value) {
 		if (rawData != null) {
 			throw new InvalidParameterException("Data cannot be used at the same time as fields");
 		}
-
+	
 		fields.add(new Field(name, value, null));
 		return this;
 	}
 
+	/**
+	 * Add a text field or attach file.  If value is not a file then it must be easily converted to a string.
+	 * 
+	 * <p>If urlEncodedForm() or dataForm() have not been called then the form type will be auto selected:
+	 * if field containing a file has been added then content type will be set to "multipart/form-data"
+	 * else it will be set to "application/x-www-form-urlencoded"</p>
+	 */
 	public HttpEasy field(String name, Object value, MediaType type) {
 		if (rawData != null) {
 			throw new InvalidParameterException("Data cannot be used at the same time as fields");
 		}
 
 		fields.add(new Field(name, value, type));
+		return this;
+	}
+	
+	/**
+	 * Sets the content type request property to the value of the supplied media type.
+	 * This can only be called once as passing raw data can only support one text block or binary file.
+	 * With this in mind files are supported but any other objects must be easily converted to a string value.
+	 */
+	public HttpEasy data(Object data, MediaType mediaType) {
+		if (rawData != null) {
+			throw new InvalidParameterException("Only a single data value can be added");
+		}
+
+		if (this.dataContentType != DataContentType.AUTO_SELECT) {
+			throw new InvalidParameterException("Content type cannot be changed once set");
+		}
+
+		dataContentType = DataContentType.RAW;
+		rawDataMediaType = mediaType;
+		rawData = data;
+
+		return this;
+	}
+
+	/**
+	 * Add a list of response codes to ignore that would otherwise case a exception to be thrown
+	 * eg doNotFailOn(HttpURLConnection.HTTP_CONFLICT)
+	 */
+	public HttpEasy doNotFailOn(Integer... reponseCodes) {
+		this.ignoreResponseCodes.addAll(Arrays.asList(reponseCodes));
+		return this;
+	}
+
+	
+	/**
+	 * Add a list of response family codes to ignore that would otherwise case a exception to be thrown
+	 * eg doNotFailOn(Family.REDIRECTION)
+	 */
+	public HttpEasy doNotFailOn(Family... responseFamily) {
+		this.ignoreResponseFamily.addAll(Arrays.asList(responseFamily));
 		return this;
 	}
 
@@ -218,20 +295,8 @@ public class HttpEasy {
 		connection.setReadTimeout(15*1000);	// give it 15 seconds to respond
 		connection.setInstanceFollowRedirects(false);
 		
-		boolean isPost = requestMethod.equals("POST") || requestMethod.equals("PUT");
-		
-		if (isPost) {
-			if (rawData != null) {
-				dataWriter = new RawDataWriter(connection, rawData, rawDataMediaType);
-			}
-			
-			if (!fields.isEmpty()) {
-				if (fieldsHasFile()) {
-					dataWriter = new FormDataWriter(connection, url.getQuery(), fields);
-				} else {
-					dataWriter = new FormUrlEncodedDataWriter(connection, url.getQuery(), fields);
-				}
-			}
+		if (requestMethod.equals("POST") || requestMethod.equals("PUT")) {
+			dataWriter = getDataWriter(dataWriter, url, connection);
 			
 			connection.setDoOutput(true);
 		}
@@ -244,6 +309,39 @@ public class HttpEasy {
 		}
 		
 		return connection;
+	}
+
+	private DataWriter getDataWriter(DataWriter dataWriter, URL url, HttpURLConnection connection) throws UnsupportedEncodingException {
+		if (dataContentType == DataContentType.AUTO_SELECT) {
+			if (!fields.isEmpty()) {
+				if (fieldsHasFile()) {
+					dataContentType = DataContentType.FORM_DATA;
+				} else {
+					dataContentType = DataContentType.X_WWW_FORM_URLENCODED;
+				}
+			}
+		}
+		
+		switch (dataContentType) {
+			case RAW:
+				dataWriter = new RawDataWriter(connection, rawData, rawDataMediaType);
+				break;
+				
+			case FORM_DATA:
+				dataWriter = new FormDataWriter(connection, url.getQuery(), fields);
+				break;
+			
+			case X_WWW_FORM_URLENCODED:
+				dataWriter = new FormUrlEncodedDataWriter(connection, url.getQuery(), fields);
+				break;
+				
+			case AUTO_SELECT:
+				break;
+				
+			default:
+				throw new InvalidParameterException(dataContentType.toString() + " is unknown");
+		}
+		return dataWriter;
 	}
 
 	private boolean fieldsHasFile() {
@@ -358,5 +456,7 @@ public class HttpEasy {
 		connection.setRequestProperty("Proxy-Authorization", proxyAuthString);
 	}
 
-	
+	private enum DataContentType {
+		AUTO_SELECT, RAW, X_WWW_FORM_URLENCODED, FORM_DATA; 
+	}
 }
