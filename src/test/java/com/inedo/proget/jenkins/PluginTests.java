5939034e2910b3bea7897572f22a8fd2fb5177e9
Andrew Sumner <andrew.sumner@xtra.co.nz>
2016-05-01 15:49:05 +1200

    Working test that exercised the download plugin

--- src/test/java/com/inedo/proget/jenkins/PluginTests.java
+++ src/test/java/com/inedo/proget/jenkins/PluginTests.java
@@ -1,96 +1,115 @@
 package com.inedo.proget.jenkins;
-////------------------------------------------------------------------------
-//// TODO This is a nearly there, just need to get it passing
-////------------------------------------------------------------------------
-//
-//package com.inedo.proget.jenkins;
-//
-//import com.inedo.TestConfig;
-//import com.inedo.proget.MockServer;
-//import com.inedo.proget.api.ProGetConfig;
-//import hudson.EnvVars;
-//import hudson.Launcher;
-//import hudson.model.*;
-//import hudson.slaves.EnvironmentVariablesNodeProperty;
-//import org.apache.commons.io.FileUtils;
-//import org.junit.*;
-//import org.junit.rules.TemporaryFolder;
-//import org.jvnet.hudson.test.JenkinsRule;
-//import org.jvnet.hudson.test.TestBuilder;
-//
-//import java.io.IOException;
-//
-//import static org.hamcrest.Matchers.*;
-//import static org.junit.Assert.assertThat;
-//
-///**
-// * Tests for the TriggerBuildHelper class
-// *
-// * @author Andrew Sumner
-// */
-//public class DownloadPluginTests {
-//	private MockServer mockServer = null;
-//
-//	@Rule public TemporaryFolder folder = new TemporaryFolder();
-//    @ClassRule public static JenkinsRule j = new JenkinsRule();
-//
-//	@Before
-//	public void before() throws IOException, InterruptedException {
-//        ProGetConfig config;
-//
-//        if (TestConfig.useMockServer()) {
-//            mockServer = new MockServer();
-//            config = mockServer.getProGetConfig();
-//        } else {
-//            config = TestConfig.getProGetConfig();
-//        }
-//
-//        // TODO Look at using Mockito to get global configuration rather than injecting it
-//        ProGetHelper.injectConfiguration(config);
-//	}
-//
-//	@After
-//	public void tearDown() throws Exception {
-//        if (mockServer != null) {
-//            mockServer.stop();
-//        }
-//
-//        ProGetHelper.injectConfiguration(null);
-//	}
-//
-//	@Test
-//	public void performDownload() throws Exception {
-//        // TODO doesn't point at valid file
-//        String feedName = "Example";
-//        String groupName = "andrew/sumner/example";
-//        String packageName = "examplepackage";
-//        String version = "0.0.1";
-//        DownloadPackageBuilder.DownloadFormat downloadFormat = DownloadPackageBuilder.DownloadFormat.PACKAGE;
-//        String downloadFolder = folder.getRoot().getAbsolutePath();
-//
-//        FreeStyleProject project = j.createFreeStyleProject();
-//
-//        // TODO This belongs in upload rather than download
-//        project.getBuildersList().add(new TestBuilder() {
-//            @Override
-//            public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-//                build.getWorkspace().child("abc.txt").write("hello", "UTF-8");
-//                return true;
-//            }
-//        });
-//
-//        project.getBuildersList().add(new DownloadPackageBuilder(feedName, groupName, packageName, version, downloadFormat.getFormat(), downloadFolder));
-//
-//        FreeStyleBuild build = project.scheduleBuild2(0).get();
-//
-//        assertThat("Result should be successful", build.getResult() , is(Result.SUCCESS));
-//
-//        String log = FileUtils.readFileToString(build.getLogFile());
-//        assertThat(log, containsString("+ echo hello"));
-//
-//        EnvironmentVariablesNodeProperty prop = j.jenkins.getGlobalNodeProperties().get(EnvironmentVariablesNodeProperty.class);
-//        EnvVars envVars = prop.getEnvVars();
-//        String file = envVars.get("PROGET_FILE");
-//        assertThat(file, is(notNullValue()));
-//	}
-//}
+//------------------------------------------------------------------------
+// TODO This is a nearly there, just need to get it passing
+//------------------------------------------------------------------------
+
+import com.inedo.utils.TestConfig;
+import com.inedo.utils.MockServer;
+import com.inedo.proget.api.ProGetConfig;
+import hudson.EnvVars;
+import hudson.Launcher;
+import hudson.model.*;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.TestBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for the TriggerBuildHelper class
+ *
+ * @author Andrew Sumner
+ */
+public class PluginTests {
+	private MockServer mockServer = null;
+
+	@Rule public TemporaryFolder folder = new TemporaryFolder();
+    @ClassRule public static JenkinsRule j = new JenkinsRule();
+
+	@Before
+	public void before() throws IOException, InterruptedException {
+        ProGetConfig config;
+
+        if (TestConfig.useMockServer()) {
+            mockServer = new MockServer();
+            config = mockServer.getProGetConfig();
+        } else {
+            config = TestConfig.getProGetConfig();
+        }
+
+        // TODO Look at using Mockito to get global configuration rather than injecting it
+        ProGetHelper.injectConfiguration(config);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+        if (mockServer != null) {
+            mockServer.stop();
+        }
+
+        ProGetHelper.injectConfiguration(null);
+	}
+
+	@Test
+	public void performDownload() throws Exception {
+        // TODO doesn't point at valid file
+        String feedName = "Example";
+        String groupName = "andrew/sumner/proget";
+        String packageName = "ExamplePackage";
+        String version = "0.0.3";
+        DownloadPackageBuilder.DownloadFormat downloadFormat = DownloadPackageBuilder.DownloadFormat.PACKAGE;
+        String downloadFolder = folder.getRoot().getAbsolutePath();
+
+        FreeStyleProject project = j.createFreeStyleProject();
+
+        // TODO This belongs in upload rather than download
+        project.getBuildersList().add(new TestBuilder() {
+            @Override
+            public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+                build.getWorkspace().child("abc.txt").write("hello", "UTF-8");
+                return true;
+            }
+        });
+
+        project.getBuildersList().add(new DownloadPackageBuilder(feedName, groupName, packageName, version, downloadFormat.getFormat(), downloadFolder));
+
+        final HoldFileName progetFile = new HoldFileName();
+        
+        project.getBuildersList().add(new TestBuilder() {
+            @Override
+            public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+            	progetFile.fileName = build.getEnvironment(listener).expand("${PROGET_FILE}");
+                return true;
+            }
+        });
+
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+
+        assertThat("Result is successful", build.getResult() , is(Result.SUCCESS));
+
+        String log = FileUtils.readFileToString(build.getLogFile());
+        assertThat("Has logged ProGet actions", log, containsString("[ProGet]"));
+
+        assertThat("PROGET_FILE variable has been set", progetFile.fileName, is(notNullValue()));
+        assertThat("PROGET_FILE variable points to existing file", new File(downloadFolder, progetFile.fileName).exists(), is(true));
+	}
+
+	
+	public void setEnvironmentVariables() throws IOException {
+	    EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
+	    EnvVars envVars = prop.getEnvVars();
+	    envVars.put("sampleEnvVarKey", "sampleEnvVarValue");
+	    j.jenkins.getGlobalNodeProperties().add(prop);
+	}
+
+	public class HoldFileName {
+		public String fileName;
+	}
+}
