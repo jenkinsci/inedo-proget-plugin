4c1c796939328237c14c12e2b4760d1cfaa528cc
Andrew Sumner <andrew.sumner@xtra.co.nz>
2016-05-01 14:54:19 +1200

    Mock unit tests

--- src/main/java/com/inedo/proget/domain/Version.java
+++ src/main/java/com/inedo/proget/domain/Version.java
@@ -2,18 +2,19 @@ package com.inedo.proget.domain;
 
 public class Version
 {
-//	[
-//	  {
-//	    "ProGetPackage_Id": 1,
-//	    "Version_Text": "0.0.1",
-//	    "PackageHash_Bytes": "tumX5KJipjnvfk7SE+LRI2bREqQ\u003d",
-//	    "PackageMetadata_Bytes": "eyJ0aXRsZSI6ImRpc3RyaWJ1dGlvbiBmaWxlcyBmb3IgZXhhbXBsZXBhY2thZ2UiLCJpY29uIjoicGFja2FnZTovL21vdGlmLW11bHRpLW1vYmlsZS5zdmciLCJkZXNjcmlwdGlvbiI6IlRoaXMgY29udGFpbnMgW2FzdCBkaXN0cm9dKGh0dHA6Ly9pbml0cm9kZS1uZXQubG9jYWwvYXN0KSBmaWxlcyBzcGVjaWZpYyB0byBBQkwiLCJkZXBlbmRlbmNpZXMiOlsiaW5pdHJvZGUvdmVuZG9ycy1jb21tb246YXN0LWNvbW1vbjoyLjAuMCJdfQ\u003d\u003d",
-//	    "Published_Date": "2016-04-20T14:24:41.13",
-//	    "Package_Size": 1005920,
-//	    "Download_Count": 56,
-//	    "Cached_Indicator": false
-//	  }
-//	]
+	public static final String MULTIPLE =
+	"[" +
+	"  {" +
+	"    \"ProGetPackage_Id\": 1," +
+	"    \"Version_Text\": \"0.0.1\"," +
+	"    \"PackageHash_Bytes\": \"tumX5KJipjnvfk7SE+LRI2bREqQ\u003d\"," +
+	"    \"PackageMetadata_Bytes\": \"eyJ0aXRsZSI6ImRpc3RyaWJ1dGlvbiBmaWxlcyBmb3IgZXhhbXBsZXBhY2thZ2UiLCJpY29uIjoicGFja2FnZTovL21vdGlmLW11bHRpLW1vYmlsZS5zdmciLCJkZXNjcmlwdGlvbiI6IlRoaXMgY29udGFpbnMgW2FzdCBkaXN0cm9dKGh0dHA6Ly9pbml0cm9kZS1uZXQubG9jYWwvYXN0KSBmaWxlcyBzcGVjaWZpYyB0byBBQkwiLCJkZXBlbmRlbmNpZXMiOlsiaW5pdHJvZGUvdmVuZG9ycy1jb21tb246YXN0LWNvbW1vbjoyLjAuMCJdfQ\u003d\u003d\"," +
+	"    \"Published_Date\": \"2016-04-20T14:24:41.13\"," +
+	"    \"Package_Size\": 1005920," +
+	"    \"Download_Count\": 56," +
+	"    \"Cached_Indicator\": false" +
+	"  }" +
+	"]";
 	
 	public String Version_Text;
 }
--- src/test/java/com/inedo/utils/MockServer.java
+++ src/test/java/com/inedo/utils/MockServer.java
@@ -1,5 +1,6 @@
 package com.inedo.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URI;
@@ -7,6 +8,8 @@ import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.bootstrap.HttpServer;
 import org.apache.http.impl.bootstrap.ServerBootstrap;
@@ -15,6 +18,8 @@ import org.apache.http.protocol.HttpRequestHandler;
 
 import com.inedo.proget.api.ProGetConfig;
 import com.inedo.proget.domain.Feed;
+import com.inedo.proget.domain.ProGetPackage;
+import com.inedo.proget.domain.Version;
 
 /**
  * A Mocked server that replaces a live ProGet installation
@@ -38,6 +43,9 @@ public class MockServer {
 
 		config = new ProGetConfig();
 		config.url = "http://" + server.getInetAddress().getHostName() + ":" + server.getLocalPort();
+		config.apiKey = "1";
+		config.user = "User";
+		config.password = "Password";
 	}
 	
 	public ProGetConfig getProGetConfig() {
@@ -61,7 +69,36 @@ public class MockServer {
 			
 			switch (method) {
 			case "Feeds_GetFeeds":
-				response.setEntity(new StringEntity(Feed.EXAMPLE));
+				response.setEntity(new StringEntity(Feed.MULTIPLE));
+				break;
+				
+			case "Feeds_GetFeed":
+				response.setEntity(new StringEntity(Feed.SINGLE));
+				break;
+				
+			case "ProGetPackages_GetPackages":
+				response.setEntity(new StringEntity(ProGetPackage.MULTIPLE));
+				break;
+
+			case "ProGetPackages_GetPackageVersions":
+				response.setEntity(new StringEntity(Version.MULTIPLE));
+				break;
+				
+			case "/upack/Example/upload":
+				response.setStatusCode(HttpStatus.SC_OK);
+				break;
+				
+			case "/upack/Default/download/andrew/sumner/example/examplepackage/0.0.1":
+				try {
+					File file = new File("src/test/resources/com/inedo/proget/api/example-0.0.1.upack");
+					FileEntity body = new FileEntity(file, ContentType.DEFAULT_BINARY);
+	                response.setHeader("Content-Type", "application/force-download");
+	                response.setHeader("Content-Disposition","attachment; filename=example-0.0.1.upack");
+	                response.setEntity(body);
+				} catch (Exception e) {
+					System.err.println(e);
+				}
+                
 				break;
 				
 			default:
@@ -69,6 +106,5 @@ public class MockServer {
 				response.setEntity(new StringEntity("API method " + method + " not found."));
 			}
 		}
-		
 	}
 }
